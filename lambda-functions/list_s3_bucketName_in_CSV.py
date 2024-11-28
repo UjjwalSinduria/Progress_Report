4547d309e61a1eb6b8c1acfbf860517d082e3f6d
Ujjwal <ujjwal.i@rapyder.com>
2024-11-28 11:46:44 +0530

    Task12: Get the list of all Security Groups inside all the VPCs. Take a VPC that has maximum number SGs.

--- lambda-functions/list_s3_bucketName_in_CSV.py
+++ lambda-functions/list_s3_bucketName_in_CSV.py
@@ -1,12 +1,28 @@
 import boto3
-import csv
-from io import StringIO
+
+client = boto3.client('s3')
 
 def lambda_handler(event,context):
-    s3 = boto3.client('s3')
-    response = s3.list_buckets()
     result = []
+    response = client.list_buckets()
     for res in response['Buckets']:
         result.append(res['Name'])
-        
-    print(result)
+    print(result[0])
+    # amit-s3demo
+    bucket_name = "amit-s3demo"
+    file_name="list_content.csv"
+   
+   
+
+    client.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=','.join(result)
+    )
+
+
+
+
+
+
+    
--- lambda-functions/list_s3_buckets.py
+++ lambda-functions/list_s3_buckets.py
@@ -1,28 +1,11 @@
 import boto3
 
+
 client = boto3.client('s3')
-
 def lambda_handler(event,context):
-    result = []
     response = client.list_buckets()
-    for res in response['Buckets']:
-        result.append(res['Name'])
-    print(result[0])
-    # amit-s3demo
-    bucket_name = "amit-s3demo"
-    file_name="list_content.csv"
-   
-   
-
-    client.put_object(
-        Bucket=bucket_name,
-        Key=file_name,
-        Body=','.join(result)
-    )
-
-
-
-
-
-
-    
+    try:
+        for bucket in response['Buckets']:
+            print(bucket['Name'])
+    except:
+        print('NO response found')
